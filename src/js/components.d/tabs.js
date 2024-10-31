9850dfb7105f701e4b410e6b93a4ecb065b4939d
Dis <dis@itdv.pro>
2024-10-31 08:27:44 +0300

    upd

--- src/js/components.d/tabs.js
+++ src/js/components.d/tabs.js
@@ -16,3 +16,48 @@ document.addEventListener('click', (event) => {
         tabs.querySelector(`[data-tabs-target="${tabsPath}"]`).classList.add('active');
     }
 });
+
+function tabSelect() {
+
+    document.addEventListener('click', (event) => {
+        if (event.target.closest('[data-select-toggle]')) {
+            let selectContainer = event.target.closest('[data-select]');
+            if (selectContainer.classList.contains('open')) {
+                selectContainer.classList.remove('open');
+            }
+            else {
+                selectClose();
+                selectContainer.classList.add('open');
+            }
+        }
+        else {
+            if (!event.target.closest('[data-select-content]')) {
+                selectClose();
+            }
+        }
+        function selectClose() {
+            document.querySelectorAll('[data-select]').forEach(el => {
+                el.classList.remove('open');
+            });
+        }
+    });
+
+    document.addEventListener('click', (event) => {
+        if (event.target.closest('[data-select-item]')) {
+            console.log('select')
+            const select        = event.target.closest('[data-select]')
+            const selectActive  = select.querySelector('[data-select-active]')
+
+            let selectItem= event.target.closest('[data-select-item]').innerHTML
+
+            select.querySelectorAll('[data-select-item]').forEach(elem => {
+                elem.classList.remove('active');
+            });
+            event.target.closest('[data-select-item]').classList.add('active')
+            select.classList.remove('open')
+            selectActive.innerHTML = selectItem
+        }
+    });
+}
+
+tabSelect()
